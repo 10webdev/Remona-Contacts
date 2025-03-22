12f47f4fe34cae794ef679cfc805c47ba1a7a3f5
10webdev <the10webdev@gmail.com>
2025-03-22 07:46:06 +0200

    Update business-script.js

--- business-script.js
+++ business-script.js
@@ -1,9 +1,9 @@
 // Sample Articles Data
 const articles = [
     {
-        title: "The 10 Web Developers (Pty) Ltd.",
-        image: "image1.png",
-        text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
+                title: "The 10 Web Developers (Pty) Ltd.",
+                image: "img/image1.webp",
+                text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
 
 The '10' has partnered with skilled professionals to combines expertise in Web Development and Google Business Profile Optimization to provide comprehensive services, including website design, content creation, and targeted advertising, digital designs, and SEO among others.
 
@@ -32,12 +32,12 @@ tinyurl.com/10webdev
         **Sat:** 08:00 - 14:00
         **Sun:** Online Only
         `,
-    },
+        },
 
-    {
-        title: "Remona Contacts",
-        image: "image2.png",
-        text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
+        {
+                title: "Remona Contacts",
+                image: "img/image2.webp",
+                text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
 
         **Industry**
 Information
@@ -55,12 +55,12 @@ www.remonacontacts.co.za
         **Operating Hours**
         **Mon - Sun:** 24 Hours
         `,
-    },
+        },
 
-    {
-        title: "The 10 Web Developers (Pty) Ltd.",
-        image: "image1.png",
-        text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
+        {
+                title: "The 10 Web Developers (Pty) Ltd.",
+                image: "img/image1.webp",
+                text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
 
 The '10' has partnered with skilled professionals to combines expertise in Web Development and Google Business Profile Optimization to provide comprehensive services, including website design, content creation, and targeted advertising, digital designs, and SEO among others.
 
@@ -89,12 +89,12 @@ tinyurl.com/10webdev
         **Sat:** 08:00 - 14:00
         **Sun:** Online Only
         `,
-    },
+        },
 
-    {
-        title: "Remona Contacts",
-        image: "image2.png",
-        text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
+        {
+                title: "Remona Contacts",
+                image: "img/image2.webp",
+                text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
 
         **Industry**
 Information
@@ -112,12 +112,12 @@ www.remonacontacts.co.za
         **Operating Hours**
         **Mon - Sun:** 24 Hours
         `,
-    },
+        },
 
-    {
-        title: "The 10 Web Developers (Pty) Ltd.",
-        image: "image1.png",
-        text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
+        {
+                title: "The 10 Web Developers (Pty) Ltd.",
+                image: "img/image1.webp",
+                text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
 
 The '10' has partnered with skilled professionals to combines expertise in Web Development and Google Business Profile Optimization to provide comprehensive services, including website design, content creation, and targeted advertising, digital designs, and SEO among others.
 
@@ -146,12 +146,12 @@ tinyurl.com/10webdev
         **Sat:** 08:00 - 14:00
         **Sun:** Online Only
         `,
-    },
+        },
 
-    {
-        title: "Remona Contacts",
-        image: "image2.png",
-        text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
+        {
+                title: "Remona Contacts",
+                image: "img/image2.webp",
+                text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
 
         **Industry**
 Information
@@ -169,12 +169,12 @@ www.remonacontacts.co.za
         **Operating Hours**
         **Mon - Sun:** 24 Hours
         `,
-    },
+        },
 
-    {
-        title: "The 10 Web Developers (Pty) Ltd.",
-        image: "image1.png",
-        text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
+        {
+                title: "The 10 Web Developers (Pty) Ltd.",
+                image: "img/image1.webp",
+                text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
 
 The '10' has partnered with skilled professionals to combines expertise in Web Development and Google Business Profile Optimization to provide comprehensive services, including website design, content creation, and targeted advertising, digital designs, and SEO among others.
 
@@ -203,12 +203,12 @@ tinyurl.com/10webdev
         **Sat:** 08:00 - 14:00
         **Sun:** Online Only
         `,
-    },
+        },
 
-    {
-        title: "Remona Contacts",
-        image: "image2.png",
-        text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
+        {
+                title: "Remona Contacts",
+                image: "img/image2.webp",
+                text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
 
         **Industry**
 Information
@@ -226,12 +226,12 @@ www.remonacontacts.co.za
         **Operating Hours**
         **Mon - Sun:** 24 Hours
         `,
-    },
+        },
 
-    {
-        title: "The 10 Web Developers (Pty) Ltd.",
-        image: "image1.png",
-        text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
+        {
+                title: "The 10 Web Developers (Pty) Ltd.",
+                image: "img/image1.webp",
+                text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
 
 The '10' has partnered with skilled professionals to combines expertise in Web Development and Google Business Profile Optimization to provide comprehensive services, including website design, content creation, and targeted advertising, digital designs, and SEO among others.
 
@@ -260,12 +260,12 @@ tinyurl.com/10webdev
         **Sat:** 08:00 - 14:00
         **Sun:** Online Only
         `,
-    },
+        },
 
-    {
-        title: "Remona Contacts",
-        image: "image2.png",
-        text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
+        {
+                title: "Remona Contacts",
+                image: "img/image2.webp",
+                text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
 
         **Industry**
 Information
@@ -283,12 +283,10 @@ www.remonacontacts.co.za
         **Operating Hours**
         **Mon - Sun:** 24 Hours
         `,
-    },
-
-    {
-        title: "The 10 Web Developers (Pty) Ltd.",
-        image: "image1.png",
-        text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
+        },{
+                title: "The 10 Web Developers (Pty) Ltd.",
+                image: "img/image1.webp",
+                text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
 
 The '10' has partnered with skilled professionals to combines expertise in Web Development and Google Business Profile Optimization to provide comprehensive services, including website design, content creation, and targeted advertising, digital designs, and SEO among others.
 
@@ -317,12 +315,12 @@ tinyurl.com/10webdev
         **Sat:** 08:00 - 14:00
         **Sun:** Online Only
         `,
-    },
+        },
 
-    {
-        title: "Remona Contacts",
-        image: "image2.png",
-        text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
+        {
+                title: "Remona Contacts",
+                image: "img/image2.webp",
+                text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
 
         **Industry**
 Information
@@ -340,5 +338,233 @@ www.remonacontacts.co.za
         **Operating Hours**
         **Mon - Sun:** 24 Hours
         `,
-    }
+        },
+
+        {
+                title: "The 10 Web Developers (Pty) Ltd.",
+                image: "img/image1.webp",
+                text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
+
+The '10' has partnered with skilled professionals to combines expertise in Web Development and Google Business Profile Optimization to provide comprehensive services, including website design, content creation, and targeted advertising, digital designs, and SEO among others.
+
+Our mission is to empower your brand with a strong online presence and create meaningful connections with your target audience. Together, we can navigate the ever-evolving digital world and achieve your business goals.
+
+        **Industry**
+Information Technology
+
+        **Address**
+T10 Bluegumbosch
+Phuthaditjhaba
+9869
+
+        **Phone Number**
++27 71 290 5286
+
+        **Website**
+tinyurl.com/10webdev
+
+        **Operating Hours**
+        **Mon:** 08:00 - 18:00
+        **Tue:** 08:00 - 18:00
+        **Wed:** 08:00 - 18:00
+        **Thu:** 08:00 - 18:00
+        **Fri:** 08:00 - 18:00
+        **Sat:** 08:00 - 14:00
+        **Sun:** Online Only
+        `,
+        },
+
+        {
+                title: "Remona Contacts",
+                image: "img/image2.webp",
+                text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
+
+        **Industry**
+Information
+
+        **Address**
+T10 Bluegumbosch
+Phuthaditjhaba
+9869
+
+        **Phone Number**
++27 71 290 5286
+
+        **Website**
+www.remonacontacts.co.za
+        **Operating Hours**
+        **Mon - Sun:** 24 Hours
+        `,
+        },
+
+        {
+                title: "The 10 Web Developers (Pty) Ltd.",
+                image: "img/image1.webp",
+                text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
+
+The '10' has partnered with skilled professionals to combines expertise in Web Development and Google Business Profile Optimization to provide comprehensive services, including website design, content creation, and targeted advertising, digital designs, and SEO among others.
+
+Our mission is to empower your brand with a strong online presence and create meaningful connections with your target audience. Together, we can navigate the ever-evolving digital world and achieve your business goals.
+
+        **Industry**
+Information Technology
+
+        **Address**
+T10 Bluegumbosch
+Phuthaditjhaba
+9869
+
+        **Phone Number**
++27 71 290 5286
+
+        **Website**
+tinyurl.com/10webdev
+
+        **Operating Hours**
+        **Mon:** 08:00 - 18:00
+        **Tue:** 08:00 - 18:00
+        **Wed:** 08:00 - 18:00
+        **Thu:** 08:00 - 18:00
+        **Fri:** 08:00 - 18:00
+        **Sat:** 08:00 - 14:00
+        **Sun:** Online Only
+        `,
+        },
+
+        {
+                title: "Remona Contacts",
+                image: "img/image2.webp",
+                text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
+
+        **Industry**
+Information
+
+        **Address**
+T10 Bluegumbosch
+Phuthaditjhaba
+9869
+
+        **Phone Number**
++27 71 290 5286
+
+        **Website**
+www.remonacontacts.co.za
+        **Operating Hours**
+        **Mon - Sun:** 24 Hours
+        `,
+        },
+
+        {
+                title: "The 10 Web Developers (Pty) Ltd.",
+                image: "img/image1.webp",
+                text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
+
+The '10' has partnered with skilled professionals to combines expertise in Web Development and Google Business Profile Optimization to provide comprehensive services, including website design, content creation, and targeted advertising, digital designs, and SEO among others.
+
+Our mission is to empower your brand with a strong online presence and create meaningful connections with your target audience. Together, we can navigate the ever-evolving digital world and achieve your business goals.
+
+        **Industry**
+Information Technology
+
+        **Address**
+T10 Bluegumbosch
+Phuthaditjhaba
+9869
+
+        **Phone Number**
++27 71 290 5286
+
+        **Website**
+tinyurl.com/10webdev
+
+        **Operating Hours**
+        **Mon:** 08:00 - 18:00
+        **Tue:** 08:00 - 18:00
+        **Wed:** 08:00 - 18:00
+        **Thu:** 08:00 - 18:00
+        **Fri:** 08:00 - 18:00
+        **Sat:** 08:00 - 14:00
+        **Sun:** Online Only
+        `,
+        },
+
+        {
+                title: "Remona Contacts",
+                image: "img/image2.webp",
+                text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
+
+        **Industry**
+Information
+
+        **Address**
+T10 Bluegumbosch
+Phuthaditjhaba
+9869
+
+        **Phone Number**
++27 71 290 5286
+
+        **Website**
+www.remonacontacts.co.za
+        **Operating Hours**
+        **Mon - Sun:** 24 Hours
+        `,
+        },
+
+        {
+                title: "The 10 Web Developers (Pty) Ltd.",
+                image: "img/image1.webp",
+                text: `As a startup, The '10' Web Developers is passionate about understanding your unique business needs and delivering exceptional results through our innovative solutions.
+
+The '10' has partnered with skilled professionals to combines expertise in Web Development and Google Business Profile Optimization to provide comprehensive services, including website design, content creation, and targeted advertising, digital designs, and SEO among others.
+
+Our mission is to empower your brand with a strong online presence and create meaningful connections with your target audience. Together, we can navigate the ever-evolving digital world and achieve your business goals.
+
+        **Industry**
+Information Technology
+
+        **Address**
+T10 Bluegumbosch
+Phuthaditjhaba
+9869
+
+        **Phone Number**
++27 71 290 5286
+
+        **Website**
+tinyurl.com/10webdev
+
+        **Operating Hours**
+        **Mon:** 08:00 - 18:00
+        **Tue:** 08:00 - 18:00
+        **Wed:** 08:00 - 18:00
+        **Thu:** 08:00 - 18:00
+        **Fri:** 08:00 - 18:00
+        **Sat:** 08:00 - 14:00
+        **Sun:** Online Only
+        `,
+        },
+
+        {
+                title: "Remona Contacts",
+                image: "img/image2.webp",
+                text: `Remona Contacts is a business details listing website that not only hosts businesses; but also offers listings for freelancers, projects, and jobs in South Africa, free of charge.
+
+        **Industry**
+Information
+
+        **Address**
+T10 Bluegumbosch
+Phuthaditjhaba
+9869
+
+        **Phone Number**
++27 71 290 5286
+
+        **Website**
+www.remonacontacts.co.za
+        **Operating Hours**
+        **Mon - Sun:** 24 Hours
+        `,
+        }
 ];
